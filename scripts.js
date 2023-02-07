fa9f82fd4d329f3a4e8191e23934324abc88ff4c
Diuri <diuri.student@gmail.com>
2023-02-07 00:36:56 -0300

    Change profile display: grid to display: flex

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
-let page = document.querySelector('.page')
+let form = document.querySelector('.form')
+let opacity = document.querySelector('#opacity-block')
 let editButton = document.querySelector('.profile__edit');
 let addButton = document.querySelector('.profile__add');
-let likeButton = document.querySelectorAll('item__like');
+let likeButton = document.querySelector('section.gallery div.item .item__like');
 
-function clickEditButton () {
-  page.classList.add('page-opacity');
-  document.body.innerHTML +=
-  `<form class="form">
-    <h1>Editar Perfil</h1>
-    <fieldset>
-      <input class="teste3" type="text" placeholder="Nome">
-      <input class="teste3 type="text" placeholder="Trabalho">
-      <button>Salvar</button>
-    </fieldset>
-  </form>`
-  ;
+function clickLikeButton () {
+  likeButton.classList.add('item__like_black');
+  console.log('teste');
 }
 
-editButton.addEventListener("click", clickEditButton)
+function clickEditButton () {
+  opacity.classList.add('page-opacity');
+  form.classList.remove('form_hidden')
+}
+
+function changeInputs () {
+  let name = document.querySelector('.profile__name');
+  let job = document.querySelector('.profile__job');
+  let newName = document.querySelector('.form__input-name');
+  let newJob = document.querySelector('.form__input-job');
+
+  if (newName.value.length > 0 && newJob.value.length > 0) {
+    name.textContent = newName.value;
+    job.textContent = newJob.value;
+    newName.value = '';
+    newJob.value = '';
+    closeForm();
+  }
+  else {
+    newName.setAttribute('placeholder', 'Preencha os campos !');
+    newJob.setAttribute('placeholder', 'Preencha os campos !');
+  }
+}
+
+function closeForm () {
+  opacity.classList.remove('page-opacity');
+  form.classList.add('form_hidden');
+}
+
+editButton.addEventListener("click", clickEditButton);
+likeButton.addEventListener("click", clickLikeButton);
