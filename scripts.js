15b999183d86a31a903ad657e44774e1933e86ab
Diuri <diuri.student@gmail.com>
2023-02-16 11:06:05 -0300

    develop/initialCards added

--- scripts.js
+++ scripts.js
@@ -5,6 +5,74 @@ const addButton = document.querySelector('.profile__add');
 const likeButton = document.querySelectorAll('.item__like');
 const exitButton = document.querySelector('.form__exit');
 
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "images/Vale-de-Yosemite.png"
+  },
+  {
+    name: "Lago Louise",
+    link: "images/Lago-Louise.png"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "images/Montanhas-Care.png"
+  },
+  {
+    name: "Latemar",
+    link: "images/Latemar.png"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "images/Vanoise-National.png"
+  },
+  {
+    name: "Lago di Braies",
+    link: "images/Lago-di-Braies.png"
+  }
+];
+
+function addInitialCards () {
+  const gallery = document.querySelector('.gallery')
+
+  for (let i = 0; i < initialCards.length; i++) {
+    const cardName = initialCards[i].name;
+    const cardLink = initialCards[i].link;
+
+    gallery.innerHTML +=
+  `<div class="item">
+    <img class="item__image" src=${cardLink}>
+    <div>
+      <h1 class="item__title">${cardName}</h1>
+      <img src="images/vector__like-button.svg" class="item__like">
+    </div>
+   </div>`
+
+  }
+}
+
+function clickAddButton () {
+
+
+  opacity.classList.add('page-opacity');
+  form.classList.remove('form_hidden');
+}
+
+function changeImageInputs () {
+  const title = document.querySelector('.item__title');
+  const url = document.querySelector('.item__url');
+  const newTitle = document.querySelector('#form-image__input-title');
+  const newUrl = document.querySelector('#form-image__input-url');
+
+  if (newTitle.value.length > 0 && newUrl.value.length > 0) {
+    title.textContent = newTitle.value;
+    url.textContent = newUrl.value;
+    newTitle.value = '';
+    newUrl.value = '';
+    closeForm();
+  }
+}
+
 function clickEditButton () {
   opacity.classList.add('page-opacity');
   form.classList.remove('form_hidden');
@@ -23,6 +91,7 @@ function changeInputs () {
     newAbout.value = '';
     closeForm();
   }
+
   else {
     newName.setAttribute('placeholder', 'Preencha os campos !');
     newAbout.setAttribute('placeholder', 'Preencha os campos !');
@@ -45,6 +114,8 @@ for (let i = 0; i < likeButton.length; i++) {
   })
 };
 
+addInitialCards();
+addButton.addEventListener('click', clickAddButton);
 editButton.addEventListener("click", clickEditButton);
 exitButton.addEventListener("click", closeForm);
 
