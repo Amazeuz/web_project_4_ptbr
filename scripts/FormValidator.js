fe725601f664794def8a2944c6fdc5e9b5cda82a
Diuri <diuri.student@gmail.com>
2023-03-23 21:31:22 -0300

    refactored FormValidator to OOP

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,69 +1,70 @@
+import * as utils from './utils.js'
+
+class FormValidator {
+  constructor(/* formList ,*/ formElement) {
+    //this._formList = formList
+    this._formElement = formElement;
+  }
+
+  _checkInputValidity (inputElement) {
+    if (!inputElement.validity.valid) {
+      inputElement.classList.add('form__input_type_error');
+      inputElement.nextElementSibling.textContent = inputElement.validationMessage;
+    }
+    else {
+      inputElement.classList.remove('form__input_type_error');
+      inputElement.nextElementSibling.textContent = ''
+    }
+  }
+
+  _hasInvalidInput (inputList) {
+    return Array.from(inputList).some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState (inputList, buttonElement) {
+    if (this._hasInvalidInput(inputList)) {
+      buttonElement.classList.add("form__button_type_inactive");
+    }
+    else {
+      buttonElement.classList.remove("form__button_type_inactive");
+    }
+  }
+
+  _insertEventListeners(fieldset) {
+    const formInputs = fieldset.querySelectorAll('.form__input');
+    const buttonElement = fieldset.querySelector('.form__button');
+    this._toggleButtonState(formInputs, buttonElement);
+
+    formInputs.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(formInputs, buttonElement);
+      });
+    });
+  }
+
+  enableValidation () {
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    const fieldsetList = this._formElement.querySelectorAll(".form__input-container");
+
+    fieldsetList.forEach((fieldset) => {
+      this._insertEventListeners(fieldset);
+    });
+
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        utils.closePopup();
+      }
+    });
+  }
+}
+
 const formList = document.forms;
 
-function pressEscKey () {
-  document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Escape') {
-      closePopup();
-    }
-  });
-}
-
-function preventSubmitRefresh (formElement) {
-  formElement.addEventListener('submit', (evt) => {
-    evt.preventDefault()
-  });
-}
-
-function checkInputValidity (inputElement) {
-  if (!inputElement.validity.valid) {
-    inputElement.classList.add('form__input_type_error');
-    inputElement.nextElementSibling.textContent = inputElement.validationMessage;
-  }
-  else {
-    inputElement.classList.remove('form__input_type_error');
-    inputElement.nextElementSibling.textContent = ''
-  }
-}
-
-function hasInvalidInput (inputList) {
-  return Array.from(inputList).some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
-
-function toggleButtonState (inputList, buttonElement) {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("form__button_type_inactive");
-  }
-  else {
-    buttonElement.classList.remove("form__button_type_inactive");
-  }
-}
-
-function insertEventListeners (formElement) {
-  const formInputs = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonElement = formElement.querySelector('.form__button');
-  toggleButtonState(formInputs, buttonElement);
-
-  formInputs.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(inputElement);
-      toggleButtonState(formInputs, buttonElement);
-    });
-  });
-}
-
-function enableValidation () {
-  const formList = document.forms;
-  Array.from(formList).forEach((formElement) => {
-    preventSubmitRefresh(formElement);
-
-    const fieldsetList = formElement.querySelectorAll(".form__input-container");
-    fieldsetList.forEach((fieldset) => {
-      insertEventListeners(fieldset);
-    });
-  });
-  pressEscKey();
-}
-
-enableValidation();
+Array.from(formList).forEach((form) => {
+  new FormValidator(form).enableValidation();
+});
